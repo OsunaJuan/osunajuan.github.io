e017e4e0c97e1babd6e77f63f54b315ca18ea79d
Juan osuna <osunajuan15@gmail.com>
2021-02-18 21:42:40 -0300

    projecto dados

---
+++ dado.js
@@ -0,0 +1,38 @@
+var boton = document.getElementById("Throw");
+var texto = document.getElementById("salida");
+var dice = document.getElementById("dice");
+var caras = document.getElementById("faces");
+var registro = document.getElementById("history");
+boton.addEventListener("click", tirarDado);
+
+var historial = [];
+registro.innerHTML="No hay jugadas";
+
+
+
+function tirarDado(){
+  console.log("help");
+  var x = parseInt(dice.value);
+  var dados=parseInt(x);
+  var temp= caras.value;
+  var max = parseInt(temp);
+
+  texto.innerHTML="";
+  
+  for(var i = 0; i<dados;i++){
+    texto.innerHTML = "|" + aleatorio(0,temp) + "|";
+    historial.push(texto.innerText);
+  }
+  registro.innerHTML="";
+  for(var i = 0;i<historial.length;i++){
+      registro.innerHTML += historial[historial.length-1-i];
+    registro.innerHTML += "<br/>";
+    
+  }   
+}
+
+function aleatorio(min,max){ // funcion que nos permite obtener numero aleatorios.
+  var resultado;
+   resultado=Math.floor(Math.random()*(max - min)+1)+min;
+  return resultado;
+}
